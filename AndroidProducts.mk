59bad564a0bd204ce2b1af3793e0b48e811b17e0
Ouroboros420 <42john42smith@googlemail.com>
2021-12-11 10:10:03 +0100

    vayu: CalyxOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vayu.mk
+    $(LOCAL_DIR)/calyx_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vayu-user \
-    lineage_vayu-userdebug \
-    lineage_vayu-eng
+    calyx_vayu-user \
+    calyx_vayu-userdebug \
+    calyx_vayu-eng
